19ab78e8e8dcf975731caf7679f30d20a77baf50
Vincas Stonys <vincaslt@gmail.com>
2023-02-05 14:03:11 +0200

    No-revalidate local api requests

--- app/api.ts
+++ app/api.ts
@@ -5,12 +5,16 @@ export async function fetchHashtags(): Promise<
     categories: Category[]
   })[]
 > {
-  const response = await fetch(`${process.env.API_URL}/hashtags`)
+  const response = await fetch(`${process.env.API_URL}/hashtags`, {
+    cache: 'no-store',
+  })
   return response.json()
 }
 
 export async function fetchCategories(): Promise<Category[]> {
-  const response = await fetch(`${process.env.API_URL}/categories`)
+  const response = await fetch(`${process.env.API_URL}/categories`, {
+    cache: 'no-store',
+  })
   return response.json()
 }
 
@@ -21,6 +25,7 @@ export async function createNewHashtag(
   const response = await fetch('api/hashtags', {
     method: 'POST',
     body: JSON.stringify({ hashtag, categories }),
+    cache: 'no-store',
   })
   return response.json()
 }
--- pages/api/hashtags.ts
+++ pages/api/hashtags.ts
@@ -1,5 +1,4 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { Hashtag } from '@prisma/client'
 import type { NextApiRequest, NextApiResponse } from 'next'
 import prisma from '../../lib/prisma'
 
